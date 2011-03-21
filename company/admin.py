080f14ff2e504337fa08aaa8b6a7c7c783f39b16
Luciano Ramalho <luciano@ramalho.org>
2011-03-20 22:57:01 -0300

    admin registration for all tables; DeptLocations and WorksOn should have composite primary keys, but this is not supported by Django 1.2.5; this has to be fixed because the database and the admin site are broken at this time; also foreign keys must be declared in the models; in some cases this was not done because the same field was supposed to be a primary key and a foreign key

--- company/admin.py
+++ company/admin.py
@@ -2,9 +2,30 @@ from django.contrib import admin
 from elnad.company.models import (Department, Dependent, DeptLocations,
     Employee, Project, WorksOn)
 
-admin.site.register(Department)
-admin.site.register(Dependent)
-admin.site.register(DeptLocations)
-admin.site.register(Employee)
-admin.site.register(Project)
-admin.site.register(WorksOn)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('dnumber', 'dname', 'mgrssn')
+
+class DependentAdmin(admin.ModelAdmin):
+    list_display = ('essn', 'dependent_name')
+
+class DeptLocationsAdmin(admin.ModelAdmin):
+    list_display = ('dnumber', 'dlocation')
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('ssn', 'lname', 'fname')
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('ssn', 'lname', 'fname')
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('pnumber', 'pname')
+
+class WorksOnAdmin(admin.ModelAdmin):
+    list_display = ('essn', 'pno')
+
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Dependent, DependentAdmin)
+admin.site.register(DeptLocations, DeptLocationsAdmin)
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(WorksOn, WorksOnAdmin)
